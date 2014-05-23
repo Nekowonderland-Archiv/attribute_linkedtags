e4f51116de562cd85401434eb35829b75c1f3634
Stefan Heimes <stefan_heimes@hotmail.com>
2014-05-23 10:56:07 +0200

    Change language files. Remove unused.

--- src/system/modules/metamodelsattribute_linkedtags/languages/en/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_linkedtags/languages/en/tl_metamodel_attribute.php
@@ -19,16 +19,16 @@
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']      = 'Display settings';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend'] = 'Display settings';
 
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags'] = 'Tags';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']           = array('Database table', 'Please select the database table.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']          = array('Table column', 'Please select the column.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']              = array('Tag ID', 'Please select a entry for the tag id.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']           = array('Tag alias', 'Please select a entry for the tag alias.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']         = array('Tag sorting', 'Please select a entry for the tag sorting.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']           = array('SQL', 'The list of options can be limited by using SQL.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']           = 'The SQL query causes an error.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['linkedtags'] = 'Linked tags';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']                 = array('Database table', 'Please select the database table.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']                = array('Table column', 'Please select the column.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']                    = array('Tag ID', 'Please select a entry for the tag id.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']                 = array('Tag alias', 'Please select a entry for the tag alias.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']               = array('Tag sorting', 'Please select a entry for the tag sorting.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']                 = array('SQL', 'The list of options can be limited by using SQL.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']                 = 'The SQL query causes an error.';
