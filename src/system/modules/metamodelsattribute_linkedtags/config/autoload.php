76764642293ce67c26a59d6bf740acb1d4add31a
Stefan Heimes <stefan_heimes@hotmail.com>
2014-10-08 23:25:03 +0200

    Update the autoload file.
    Add the new files and change the names of the old ones.

--- src/system/modules/metamodelsattribute_linkedtags/config/autoload.php
+++ src/system/modules/metamodelsattribute_linkedtags/config/autoload.php
@@ -21,10 +21,12 @@
  */
 ClassLoader::addClasses(array
 (
-	'MetaModels\Attribute\Tags\LinkedTags'                                     => 'system/modules/metamodelsattribute_linkedtags/MetaModels/Attribute/Tags/LinkedTags.php',
+	'MetaModels\Attribute\LinkedTags\LinkedTags'                               => 'system/modules/metamodelsattribute_linkedtags/MetaModels/Attribute/LinkedTags/LinkedTags.php',
+	'MetaModels\Attribute\LinkedTags\AttributeTypeFactory'                     => 'system/modules/metamodelsattribute_linkedtags/MetaModels/Attribute/LinkedTags/AttributeTypeFactory.php',
+	'MetaModels\DcGeneral\Events\MetaModels\BackendSubscriber'                 => 'system/modules/metamodelsattribute_linkedtags/MetaModels/DcGeneral/Events/MetaModels/BackendSubscriber.php',
 	'MetaModels\DcGeneral\Events\Table\Attribute\LinkedTags\PropertyAttribute' => 'system/modules/metamodelsattribute_linkedtags/MetaModels/DcGeneral/Events/Table/Attribute/LinkedTags/PropertyAttribute.php',
 	'MetaModels\Filter\Rules\FilterRuleLinkedTags'                             => 'system/modules/metamodelsattribute_linkedtags/MetaModels/Filter/Rules/FilterRuleLinkedTags.php',
-    'MetaModels\Dca\AttributeLinkedTags'                                       => 'system/modules/metamodelsattribute_linkedtags/MetaModels/Dca/AttributeLinkedTags.php',
+	'MetaModels\Dca\AttributeLinkedTags'                                       => 'system/modules/metamodelsattribute_linkedtags/MetaModels/Dca/AttributeLinkedTags.php',
 ));
 
 
